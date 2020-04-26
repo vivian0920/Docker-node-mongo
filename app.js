59cc570e7dadf0b12c866ff85efc446f5766385a
oldmo860617 <oldmo860617@gmail.com>
2020-04-27 01:59:23 +0800

    fix some docker error

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 //connect the mongodb
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://mongo:27017/connectDBapp',{useNewUrlParser:true});
+mongoose.connect('mongodb://mongo:27017/connectDBapp', { useNewUrlParser: true });
 
 //to confirm that the connection
-var db =mongoose.connection;
-db.on('error',console.error.bind(console,'connection error'));
-db.once('open',function(){
-    cinsole.log("Connection!");
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+    console.log("Connection!");
 });
 
 //define the schema
 var testSchema = new mongoose.Schema({
-    id :Number,
-    name:String
+    id: Number,
+    name: String
 });
 //to set up the allocation of Schema
-testSchema.set('collection','test');
+testSchema.set('collection', 'test');
 //default allocation
-var tesrModle=mongoose.model('test', testSchema);
+var tesrModle = mongoose.model('test', testSchema);
 
